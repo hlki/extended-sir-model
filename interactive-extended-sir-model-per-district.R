71c595505d7737257c347753712860a2ff2b868e
hlki <60828926+hlki@users.noreply.github.com>
2024-03-19 22:51:27 +0200

    add improvments

--- interactive-extended-sir-model-per-district.R
+++ interactive-extended-sir-model-per-district.R
@@ -83,8 +83,8 @@ server <- function(input, output, session) {
         print(formatted_output)
         
         # Differential equations
-        dS <- -beta * S * I / N - nu_value * S
-        dI <- beta * S * I / N - gamma * I
+        dS <- -beta * (S * I / N) - nu_value * S
+        dI <- beta * (S * I / N) - gamma * I
         dR <- gamma * I
         dV <- nu_value * S
         
@@ -149,22 +149,25 @@ server <- function(input, output, session) {
     initial_state <- c(S = 1152400, I = 11, R = 1, V = 0)
     time_steps <- seq_len(input$time_steps)
     time_series <- ode(y = initial_state, times = time_steps, func = sir_model(), parms = input_data)
-
+    
     time_series_df <- as.data.frame(time_series)
     time_series_df$NH <- time_series_df$I * input_data$pNH
     time_series_df$NICU <- time_series_df$I * input_data$pNICU
     time_series_df$D <- time_series_df$I * input_data$m
-
+    
+    # Select every tenth point for plotting
+    thin_time_series_df <- time_series_df[seq(1, nrow(time_series_df), by = 10), ]
+    
     par(cex.lab = 1.5, font.lab = 1)
-    plot(NA, xlim = range(time_steps), ylim = range(c(time_series_df$I, observed_data$total_infected)), xlab = "Час", ylab = "Інфіковані")
-    lines(time_series_df$time, time_series_df$I, lwd = 2)
-    points(observed_data$time, observed_data$total_infected, pch = 19, col = "red")
-    legend("topleft", legend = c("Прогноз моделі", "Дані спостережень"), lty = c(1, NA), pch = c(NA, 19), col = c("black", "red"))
-
+    plot(NA, xlim = range(time_steps), ylim = range(c(thin_time_series_df$I, observed_data$total_infected)), xlab = "Час", ylab = "Інфіковані")
+    points(thin_time_series_df$time, thin_time_series_df$I, pch = 19, col = "red", cex = 1)  # Model as red points
+    lines(observed_data$time, observed_data$total_infected, col = "black", lwd = 3)  # Observed data as thicker black line
+    legend("topleft", legend = c("Прогноз моделі", "Дані спостережень"), pch = c(19, NA), lty = c(NA, 1), col = c("red", "black"), lwd = c(1, 3))
+    
     title("Порівняння прогнозованої кількості інфікованих з спостереженнями")
   })
-
-
+  
+  
   # Output for vaccinated comparison plot
   output$vaccinatedComparisonPlot <- renderPlot({
 
@@ -181,12 +184,13 @@ server <- function(input, output, session) {
     time_series_df$NICU <- time_series_df$I * input_data$pNICU
     time_series_df$D <- time_series_df$I * input_data$m
 
-    par(cex.lab = 1.5, font.lab = 1)
-    plot(NA, xlim = range(time_steps), ylim = range(c(time_series_df$V, observed_data$fully_vaccinated)), xlab = "Час", ylab = "Вакциновані")
-    lines(time_series_df$time, time_series_df$V, lwd = 2)
-    points(observed_data$time, observed_data$fully_vaccinated, pch = 19, col = "red")
-    legend("topleft", legend = c("Прогноз моделі", "Дані спостережень"), lty = c(1, NA), pch = c(NA, 19), col = c("black", "red"))
-
+    thin_vaccinated_df <- time_series_df[seq(1, nrow(time_series_df), by = 10), ]
+    
+    plot(NA, xlim = range(time_steps), ylim = range(c(thin_vaccinated_df$V, observed_data$fully_vaccinated)), xlab = "Час", ylab = "Вакциновані")
+    points(thin_vaccinated_df$time, thin_vaccinated_df$V, pch = 19, col = "red", cex = 1)  # Model as red points
+    lines(observed_data$time, observed_data$fully_vaccinated, col = "black", lwd = 3)  # Thicker black line for observed data
+    legend("topleft", legend = c("Прогноз моделі", "Дані спостережень"), pch = c(19, NA), lty = c(NA, 1), col = c("red", "black"), lwd = c(1, 2))
+    
     title("Порівняння прогнозованої кількості вакцинованих з спостереженнями")
   })
 
@@ -206,12 +210,13 @@ server <- function(input, output, session) {
     time_series_df$NICU <- time_series_df$I * input_data$pNICU
     time_series_df$D <- time_series_df$I * input_data$m
 
-    par(cex.lab = 1.5, font.lab = 1)
-    plot(NA, xlim = range(time_steps), ylim = range(c(time_series_df$R, observed_data$total_recovered)), xlab = "Час", ylab = "Одужавші")
-    lines(time_series_df$time, time_series_df$R, lwd = 2)
-    points(observed_data$time, observed_data$total_recovered, pch = 19, col = "red")
-    legend("topleft", legend = c("Прогноз моделі", "Дані спостережень"), lty = c(1, NA), pch = c(NA, 19), col = c("black", "red"))
-
+    thin_recovered_df <- time_series_df[seq(1, nrow(time_series_df), by = 10), ]
+    
+    plot(NA, xlim = range(time_steps), ylim = range(c(thin_recovered_df$R, observed_data$total_recovered)), xlab = "Час", ylab = "Одужавші")
+    points(thin_recovered_df$time, thin_recovered_df$R, pch = 19, col = "red", cex = 1)  # Model as red points
+    lines(observed_data$time, observed_data$total_recovered, col = "black", lwd = 3)  # Thicker black line for observed data
+    legend("topleft", legend = c("Прогноз моделі", "Дані спостережень"), pch = c(19, NA), lty = c(NA, 1), col = c("red", "black"), lwd = c(1, 2))
+    
     title("Порівняння прогнозованої кількості одужавших з спостереженнями")
   })
 
@@ -231,12 +236,13 @@ server <- function(input, output, session) {
     time_series_df$NICU <- time_series_df$I * input_data$pNICU
     time_series_df$D <- time_series_df$I * input_data$m
 
-    par(cex.lab = 1.5, font.lab = 1)
-    plot(NA, xlim = range(time_steps), ylim = range(c(time_series_df$D, observed_data$total_death)), xlab = "Час", ylab = "Померлі")
-    lines(time_series_df$time, time_series_df$D, lwd = 2)
-    points(observed_data$time, observed_data$total_death, pch = 19, col = "red")
-    legend("topleft", legend = c("Прогноз моделі", "Дані спостережень"), lty = c(1, NA), pch = c(NA, 19), col = c("black", "red"))
-
+    thin_death_df <- time_series_df[seq(1, nrow(time_series_df), by = 10), ]
+    
+    plot(NA, xlim = range(time_steps), ylim = range(c(thin_death_df$D, observed_data$total_death)), xlab = "Час", ylab = "Померлі")
+    points(thin_death_df$time, thin_death_df$D, pch = 19, col = "red", cex = 1)  # Model as red points
+    lines(observed_data$time, observed_data$total_death, col = "black", lwd = 3)  # Thicker black line for observed data
+    legend("topleft", legend = c("Прогноз моделі", "Дані спостережень"), pch = c(19, NA), lty = c(NA, 1), col = c("red", "black"), lwd = c(1, 2))
+    
     title("Порівняння прогнозованої кількості померлих від хвороби з спостереженнями")
   })
 }
